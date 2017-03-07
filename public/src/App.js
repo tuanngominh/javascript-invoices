08f18278b258a8d65dbbf058beb47d71a978aa51
tuanngominh <tuan@trexanhlab.com>
2017-03-07 11:28:57 +0700

    add bootstrap 3

--- public/src/App.js
+++ public/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+          <button className="btn btn-primary">Button</button>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
