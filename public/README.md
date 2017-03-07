743257a22e9de8f5c256b1f6354b533b38af43cf
tuanngominh <tuan@trexanhlab.com>
2017-03-07 12:19:20 +0700

    add redux store

--- public/README.md
+++ public/README.md
@@ -1,3 +1,5 @@
+Source code available on [github.com/tuanngominh/javascript-invoices](https://github.com/tuanngominh/javascript-invoices)
+
 # installation
 ```sh
 npm install
--- public/src/index.js
+++ public/src/index.js
@@ -4,7 +4,14 @@ import App from './App';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
+import {Provider} from 'react-redux'
+import configureStore from './store/configureStore'
+const store = configureStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
